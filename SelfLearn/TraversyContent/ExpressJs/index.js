a8761604f3c370067119bf9a47239c370d8d96e3
Utkarsh Singh <us9044@gmail.com>
2020-11-01 03:25:58 +0530

    Completed

--- SelfLearn/TraversyContent/ExpressJs/index.js
+++ SelfLearn/TraversyContent/ExpressJs/index.js
@@ -1,30 +1,33 @@
 const express = require('express')
 const path = require('path')
-
+const exphbs = require('express-handlebars')
+const members = require('./Members')
+// const logger = require('./middleware/logger')
 const app = express()
 
-const members = [
-    {
-        id: 1,
-        name: 'John Doe',
-        email: 'john@gmail.com',
-        status: 'active'
-    },
-    {
-        id: 2,
-        name: 'Bob Williams',
-        email: 'bob@gmail.com',
-        status: 'inactive'
-    }
-]
+// Init middleware
+// app.use(logger)
+
 
 // app.get('/', (req,res)=> {
 //     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 // })
 
-// Gets all members
-app.get('/api/members', (req, res) => res.json(members))
+// Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
 
+// Body Parser Middleware
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+// Homepage Route
+app.get('/', (req, res) => res.render('index',{
+    title: 'Member App',
+    members
+}))
+
+app.use('/api/members', require('./routes/api/members'))
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')))
